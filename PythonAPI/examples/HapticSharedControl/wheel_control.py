72c5f38ad5ba8d28ba20dd38c4befd6c54b14259
hiimmuc <nam1811.hust@gmail.com>
2025-02-24 06:50:48 +0900

    update data

--- PythonAPI/examples/HapticSharedControl/wheel_control.py
+++ PythonAPI/examples/HapticSharedControl/wheel_control.py
@@ -24,10 +24,12 @@ class WheelController:
             "Force feedback available:",
             self.controller.has_force_feedback(index=self.controller_index),
         )
-
+        print("Operating range:", self.controller.set_operating_range(index=self.controller_index, range=MAX_ANGLE_RANGE))
+        
     def play_spring_force(
         self, offset_percentage, saturation_percentage=0.5, coefficient_percentage=1.0
-    ):
+    ):  
+        self.controller.logi_update()
         self.controller.play_spring_force(
             index=self.controller_index,
             offset_percentage=offset_percentage,
@@ -40,7 +42,40 @@ class WheelController:
         self.controller.stop_spring_force(index=self.controller_index)
 
     def get_state_engines(self):
-        return self.controller.get_state_engines(self.controller_index).contents
+        # cspell: ignore rgdw
+        state_contents = self.controller.get_state_engines(self.controller_index).contents
+        return {
+            "lX": state_contents.lX,
+            "lY": state_contents.lY,
+            "lZ": state_contents.lZ,
+            "lRx": state_contents.lRx,
+            "lRy": state_contents.lRy,
+            "lRz": state_contents.lRz,
+            "rglSlider": list(state_contents.rglSlider),
+            "rgdwPOV": list(state_contents.rgdwPOV),
+            "rgbButtons": list(state_contents.rgbButtons),
+            "lVX": state_contents.lVX,
+            "lVY": state_contents.lVY,
+            "lVZ": state_contents.lVZ,
+            "lVRx": state_contents.lVRx,
+            "lVRy": state_contents.lVRy,
+            "lVRz": state_contents.lVRz,
+            "rglVSlider": list(state_contents.rglVSlider),
+            "lAX": state_contents.lAX,
+            "lAY": state_contents.lAY,
+            "lAZ": state_contents.lAZ,
+            "lARx": state_contents.lARx,
+            "lARy": state_contents.lARy,
+            "lARz": state_contents.lARz,
+            "rglASlider": list(state_contents.rglASlider),
+            "lFX": state_contents.lFX,
+            "lFY": state_contents.lFY,
+            "lFZ": state_contents.lFZ,
+            "lFRx": state_contents.lFRx,
+            "lFRy": state_contents.lFRy,
+            "lFRz": state_contents.lFRz,
+            "rglFSlider": list(state_contents.rglFSlider),
+        }
 
     def get_angle(self, translate=True):
         """
@@ -55,9 +90,9 @@ class WheelController:
         """
 
         if not translate:
-            return self.get_state_engines().lX
+            return self.get_state_engines()["lX"]
         else:
-            return np.clip(self.get_state_engines().lX, -32767.0, 32767.0) / 32767.0
+            return np.clip(self.get_state_engines()["lX"], -32767.0, 32767.0) / 32767.0
 
     def get_acceleration_pedal(self, translate=True):
         """
@@ -71,9 +106,9 @@ class WheelController:
         """
 
         if not translate:
-            return self.get_state_engines().lY
+            return self.get_state_engines()["lY"]
         else:
-            return abs(self.get_state_engines().lY - 32767.0) / 65535.0
+            return abs(self.get_state_engines()["lY"] - 32767.0) / 65535.0
 
     def get_brake_pedal(self, translate=True):
         """
@@ -87,9 +122,9 @@ class WheelController:
         """
 
         if not translate:
-            return self.get_state_engines().lRZ
+            return self.get_state_engines()["lRZ"]
         else:
-            return abs(self.get_state_engines().lRZ - 32767.0) / 65535.0
+            return abs(self.get_state_engines()["lRZ"] - 32767.0) / 65535.0
 
     def get_DPad(self, translate=True):
         """
@@ -103,9 +138,9 @@ class WheelController:
         """
 
         if not translate:
-            return self.get_state_engines().rgdwPOV[0]
+            return self.get_state_engines()["rgdwPOV"][0]
         else:
-            return abs(self.get_state_engines().rgdwPOV[0] - 32767.0) / 65535.0
+            return abs(self.get_state_engines()["rgdwPOV"][0] - 32767.0) / 65535.0
 
     def exit(self):
         self.controller.steering_shutdown()
@@ -147,31 +182,33 @@ def spin_controller_full_test(controller):
 
 
 def spin_controller_forward_reverse_test(controller):
+    #NOTE: why the offset and measured is not the same?
     time.sleep(3)
     print("Forward test")
     df = {}
     print("--------------------")
-    for offset in range(-100, 101, 1):
-        print(f"Offset: {offset}", end="\n")
+    
+    for offset in range(-100, 101, 10):
+        print(f"//=====\nOffset: {offset}", end="\n")
         # -i * 45 + 90
         # -4 ~ -18
         # -45 ~ -200
-        state = controller.get_state_engines()
-        print(f"Angle before: {controller.get_angle() * 450}", end=" --> ")
+        print(f"Angle before: {round(controller.get_angle() * 450, 1)}", end=" --> ")
         controller.play_spring_force(
             offset_percentage=offset,
             saturation_percentage=100,
-            coefficient_percentage=80,
+            coefficient_percentage=100,
         )
+
         time.sleep(0.5)
         state = controller.get_state_engines()
-        
+
         # export to csv
         df.setdefault("timestamp", []).append(str(time.time()))
-        for key, value in vars(state).items():
+        for key, value in state.items():
             df.setdefault(key, []).append(value)
 
-        print(f"Angle after: {controller.get_angle() * 450}")
+        print(f"Angle after: {round(controller.get_angle() * 450, 1)}")
         print(f"Acceleration: {controller.get_acceleration_pedal()}")
         time.sleep(1)
 
@@ -179,34 +216,36 @@ def spin_controller_forward_reverse_test(controller):
     time.sleep(5)
     
     print("Reverse test")
-    for offset in range(100, -101, -1):
-        print(f"Offset: {offset}", end="\n")
+
+    for offset in range(100, -101, -10):
+        print(f"//=====\nOffset: {offset}", end="\n")
         # -i * 45 + 90
         # -4 ~ -18
         # -45 ~ -200
-        state = controller.get_state_engines()
-        print(f"Angle before: {controller.get_angle() * 450}", end=" --> ")
+        print(f"Angle before: {round(controller.get_angle() * 450, 1)}", end=" --> ")
         controller.play_spring_force(
             offset_percentage=offset,
             saturation_percentage=100,
-            coefficient_percentage=80,
+            coefficient_percentage=100,
         )
+
         time.sleep(0.5)
         state = controller.get_state_engines()
         
         # export to csv
         df.setdefault("timestamp", []).append(str(time.time()))
-        for key, value in vars(state).items():
+        for key, value in state.items():
             df.setdefault(key, []).append(value)
 
-        print(f"Angle after: {controller.get_angle() * 450}")
+        print(f"Angle after: {round(controller.get_angle() * 450, 1)}")
         print(f"Acceleration: {controller.get_acceleration_pedal()}")
         time.sleep(1)
 
     controller.stop_spring_force()
+    print(f"Angle final: {controller.get_angle() * 450}")
     time.sleep(1)
     df = pd.DataFrame().from_dict(df)
-    df.to_excel("./data/spin_test_offset_0.xlsx", index=False)
+    df.to_excel("./data/wheel_logs/spin_test_offset_backward.xlsx", index=False)
     print("Forward reverse test done.")
     time.sleep(1)
     controller.exit()
