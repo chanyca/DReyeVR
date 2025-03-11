fix: rotation direction

PythonAPI/scripts/DReyeVR_DAS.py
@@ -139,7 +139,7 @@ def main():
     world = client.get_world()
 
     sensor = DReyeVRSensor(world)
-    
+    vehicle_ego = find_ego_vehicle(world)    
     controller = WheelController()
 
     ready = True
@@ -153,15 +153,11 @@ def main():
     idx = "3"
     
     param = None
-    torques = []
-    trajectory = []
-    vehicle_yaw_angles_deg = []
 
-    steering_wheel_angles = []
-    desired_steering_angles = []
-
+    # To calibrate the steering wheel angle and steering angle
+    # find the linear relationship between steering wheel angle and steering angle
     init_sa_swa = [[], []]
-    coef = [[], []]
+    coef = [[], []]  
     
     def control_loop(data: dict) -> None:
         """This function is called every time the sensor receives new data from the simulation.
@@ -175,13 +171,10 @@ def main():
         nonlocal idx
 
         nonlocal param
-        nonlocal coef
-        nonlocal torques
-        nonlocal trajectory
-        nonlocal vehicle_yaw_angles_deg
-        nonlocal steering_wheel_angles
-        nonlocal desired_steering_angles
+
         nonlocal init_sa_swa
+        nonlocal coef
+        
         nonlocal delta_t
         nonlocal ready
         nonlocal take_control
@@ -199,7 +192,7 @@ def main():
 
             controller.play_spring_force(
                 offset_percentage=desired_offset,
-                saturation_percentage=50,
+                saturation_percentage=100,
                 coefficient_percentage=100,
             )
 
@@ -238,8 +231,8 @@ def main():
 
             position_to_world = measured_carla_data["Location"][0:2]  # [x, y]
             vehicle_yaw = measured_carla_data["Rotation"][1]  # yaw
-
-            velocity = measured_carla_data["Velocity"][0:2]  # [x, y]
+            velocity = measured_carla_data["Velocity"][0:2]  # [Vx, Vy]
+            
             speed = np.linalg.norm(velocity)
             
             # get signal from Logitech Wheel SDK, if pressed the reverse button, then reverse the steering angle
@@ -247,9 +240,6 @@ def main():
             if any([btn_value for btn_value in list(buttons.values())[1:]]):
                 backward_btn_pressed_cnt += 1
                 print("Backward button pressed")
-                
-            with open("log2_2.txt", "a") as f:
-                f.write(f"{position_to_world[0]},{position_to_world[1]},{vehicle_yaw},{speed}\n")
                 
             backward = backward_btn_pressed_cnt % 2 == 1
 
@@ -290,30 +280,25 @@ def main():
 
             # 5. if take control is allowed, then start the self driving
             if take_control:
+                # backward velocity
+                vehicle_ego.set_target_velocity(carla.Vector3D(x=-1.0, y =0.0, z=0.0))
+                
                 haptic_control = HapticSharedControl(
-                    # Cs=0.5,
-                    # Kc=0.5,
-                    # T=2,
-                    tp=2,
                     speed=speed,
                     desired_trajectory_params=param,
                     vehicle_config=vehicle_config,
+                    simulation=False
                 )
+                
                 torque, coef, desired_steering_angle_deg = haptic_control.calculate_torque(
                     current_position=position_to_world,
-                    steering_angles_deg=steering_angles,
                     current_yaw_angle_deg=vehicle_yaw,
+                    steering_angles_deg=steering_angles,
                     steering_wheel_angle_deg=steering_wheel_angle,
                 )
+                
                 desired_steering_angle_deg = np.clip(desired_steering_angle_deg, -450, 450)
                 
-                torques.append(torque)
-                trajectory.append(position_to_world)
-                steering_wheel_angles.append(steering_wheel_angle)
-
-                vehicle_yaw_angles_deg.append(vehicle_yaw)
-                desired_steering_angles.append(desired_steering_angle_deg)
-
                 try:
                     desired_offset = int(desired_steering_angle_deg * 100 / 450.0)
                 except:
@@ -325,11 +310,12 @@ def main():
                         )
                     )
                     desired_offset = int(desired_offset_deg * 100 / 450.0)
+                
                 # desired_offset = - 60
                 print(f"--> Desired Offset: {desired_offset}")
                 controller.play_spring_force(
                     offset_percentage=desired_offset,
-                    saturation_percentage=50,
+                    saturation_percentage=100,
                     coefficient_percentage=100,
                 )
                 

PythonAPI/scripts/HapticSharedControl/simulation.py
@@ -112,10 +112,10 @@ def plot_trajectory(paths, trajectories, yaw_angles_deg, recorded_path=None):
             plot_arrow(point[0], point[1], np.radians(yaw_angles_deg[i][idx]), 0.3, width=0.2)
 
     if recorded_path is not None:
-        plt.plot(recorded_path[1], recorded_path[0], label="Recorded Path", color="orange")
+        plt.plot(recorded_path[1], recorded_path[0], label="Recorded Path", color="orange", marker=".")
 
-    plt.xlabel("X")
-    plt.ylabel("Y")
+    plt.xlabel("X (m)")
+    plt.ylabel("Y (m)")
     plt.title("Vehicle Path and Bezier Trajectory")
 
     plt.legend()

PythonAPI/scripts/HapticSharedControl/utils.py
@@ -57,6 +57,14 @@ def rotation_matrix_ccw(angle_rad):
     )
 
 
+def sign(x):
+    return x / np.abs(x) if x != 0 else 0
+
+
+def linear_fn(slope, intercept):
+    # return y = slope * x + intercept
+    return lambda x: slope * x + intercept
+
 class Vehicle:
     def __init__(self, vehicle_config: dict, model: str = "simple") -> None:
         self.vehicle_config = vehicle_config
@@ -128,12 +136,12 @@ class Vehicle:
         if self.vehicle_model == "simple":
             # REF: https://www.theautopian.com/the-engineering-behind-why-some-cars-can-turn-tighter-than-others/
             steering_angle_rad = self.outer_wheel_steering_angle_rad
-            steering_angle_rad += 1e-6 if steering_angle_rad % (np.pi) == 0 else 0
+            steering_angle_rad += 1e-6 if steering_angle_rad % (np.pi) == 0 else 0 # avoid division by zero
 
             turning_radius = np.sqrt(
                 self.center_of_mass**2 + (self.wheelbase**2) / np.tan(steering_angle_rad) ** 2
             )
-            vehicle_steering_angle = np.arctan(
+            vehicle_steering_angle_rad = np.arctan(
                 self.center_of_mass_ratio * np.tan(steering_angle_rad)
             )
         else:
@@ -141,36 +149,33 @@ class Vehicle:
             # REF: https://www.ijser.org/researchpaper/optimizing-the-turning-radius-of-a-vehicle-using-symmetric.pdf
             self.inner_wheel_steering_angle_rad += (
                 1e-6 if self.inner_wheel_steering_angle_rad % (np.pi) == 0 else 0
-            )
+            ) # avoid division by zero
             self.outer_wheel_steering_angle_rad += (
                 1e-6 if self.outer_wheel_steering_angle_rad % (np.pi) == 0 else 0
-            )
-            turning_angle_rad = arccot(
+            ) # avoid division by zero
+            steering_angle_rad = arccot(
                 (
                     cot(self.inner_wheel_steering_angle_rad)
                     + cot(self.outer_wheel_steering_angle_rad)
                 )
                 / 2
             )
-            vehicle_steering_angle = np.sqrt(
-                (self.center_of_mass**2) + (self.wheelbase**2) * (cot(turning_angle_rad) ** 2)
+            vehicle_steering_angle_rad = np.sqrt(
+                (self.center_of_mass**2) + (self.wheelbase**2) * (cot(steering_angle_rad) ** 2)
             )
 
         return {
             "R": np.abs(turning_radius),
-            "Delta": np.degrees(vehicle_steering_angle),
+            "Delta": np.degrees(vehicle_steering_angle_rad),
             "CoR": np.array(
                 [
-                    self.center_of_mass / np.tan(vehicle_steering_angle),
                     -self.center_of_mass,
+                    self.center_of_mass / np.tan(vehicle_steering_angle_rad),
                 ]
-            ),
+            ), # center of rotation in vehicle coordinates [x, y] 
         }
 
 
-def linear_fn(slope, intercept):
-    # return y = slope * x + intercept
-    return lambda x: slope * x + intercept
 
 
 if __name__ == "__main__":

PythonAPI/scripts/simulation_haptic_algo.py
@@ -33,87 +33,87 @@ if trial == "0":
     P_0 = [-1.47066772, -13.22415039]  # [x, y] in carla -> [y, x] in matplotlib
     P_d = [-0.37066772, -29.02415039]
     P_f = [-6.87066772, -21.62415039]
-    yaw_0 = 0
-    yaw_d = 10
-    yaw_f = 90
+    yaw_0 = -90
+    yaw_d = -100
+    yaw_f = 180
 elif trial == "1":
     P_0 = [-2.13139057, -13.58492756]
     P_d = [-0.26648349, -27.88323593]
     P_f = [-8.6554842, -21.6907711]
-    yaw_0 = 2.3220824999999934
-    yaw_d = 29.3604774
-    yaw_f = 105.7937202
+    yaw_0 = - 2.3220824999999934 - 90
+    yaw_d = - 29.3604774 - 90
+    yaw_f = 105.7937202 + 90
 elif trial == "2":
     P_0 = [-2.0546906, -15.14080429]
     P_d = [-0.16853675, -27.82400513]
     P_f = [-7.68998575, -21.31655693]
-    yaw_0 = -1.9093017999999944
-    yaw_d = 30.0815544
-    yaw_f = 93.37516379
+    yaw_0 = -1.9093017999999944 - 90
+    yaw_d = 30.0815544 - 90
+    yaw_f = 93.37516379 + 90
 elif trial == "3":
     P_0 = [-2.33013225, -13.42747879]
     P_d = [-0.27527067, -27.43535042]
     P_f = [-7.19522429, -21.22905159]
-    yaw_0 = -1.153442400000003
-    yaw_d = 29.3213272
-    yaw_f = 90.37275675
+    yaw_0 = -1.153442400000003 -90
+    yaw_d = - 29.3213272 - 90
+    yaw_f = 90.37275675 + 90
 elif trial == "4":
     P_0 = [-2.59471703, -13.76090431]
     P_d = [-0.84754181, -27.41300201]
     P_f = [-7.63892126, -21.31984329]
-    yaw_0 = 1.9469986000000006
-    yaw_d = 25.734725999999995
-    yaw_f = 90.29345572
+    yaw_0 = - 1.9469986000000006 - 90
+    yaw_d = - 25.734725999999995 - 90
+    yaw_f = 90.29345572 + 90
 elif trial == "5":
     P_0 = [-2.46604896, -12.84768295]
     P_d = [-0.76953948, -27.44445229]
     P_f = [-7.99046612, -21.05567169]
-    yaw_0 = 3.3721313000000066
-    yaw_d = 20.9653397
-    yaw_f = 85.99301052
+    yaw_0 = - 3.3721313000000066 - 90
+    yaw_d = - 20.9653397 - 90
+    yaw_f = 85.99301052 + 90
 elif trial == "6":
     P_0 = [-2.68211842, -13.66109943]
     P_d = [-0.15471956, -27.84520721]
     P_f = [-7.58486843, -21.00892448]
-    yaw_0 = 1.5638045999999974
-    yaw_d = 31.523597700000003
-    yaw_f = 85.90444851
+    yaw_0 = - 1.5638045999999974 - 90
+    yaw_d = - 31.523597700000003 - 90
+    yaw_f = 85.90444851 + 90
 # reverse direction
 elif trial == "8":
     P_0 = [-2.21546435, -31.60477066]
     P_d = [1.03518212, -18.32664871]
     P_f = [-7.7015214, -21.18217278]
-    yaw_0 = 178.7055664
-    yaw_d = 124.3087578
-    yaw_f = 88.27893066
+    yaw_0 = 90 - (178.7055664 - 180)
+    yaw_d = 90 - (124.3087578 - 180)
+    yaw_f = 88.27893066 + 90
 elif trial == "9":
     P_0 = [-1.3271035, -34.64346695]
     P_d = [1.54469788, -19.11241913]
     P_f = [-7.67417145, -21.5524559]
-    yaw_0 = 182.4389496
-    yaw_d = 125.65747833
-    yaw_f = 88.90112317
+    yaw_0 = 182.4389496 - 90
+    yaw_d = 125.65747833 - 90
+    yaw_f = 88.90112317 + 90
 elif trial == "10":
     P_0 = [-2.41052723, -31.86808777]
     P_d = [2.61411977, -16.33299065]
     P_f = [-7.40135479, -21.47801208]
-    yaw_0 = 174.07925419999998
-    yaw_d = 125.3021584
-    yaw_f = 87.58251953
+    yaw_0 = 174.07925419999998 - 90
+    yaw_d = 125.3021584 - 90
+    yaw_f = 87.58251953 + 90
 elif trial == "11":
     P_0 = [-1.99927211, -29.94470215]
     P_d = [2.38471651, -17.51079941]
     P_f = [-7.10684538, -21.3453064]
-    yaw_0 = 181.86206049999998
-    yaw_d = 121.4338913
-    yaw_f = 88.89004517
+    yaw_0 = 181.86206049999998 - 90
+    yaw_d = 121.4338913 - 90
+    yaw_f = 88.89004517 + 90
 elif trial == "12":
     P_0 = [-3.46834612, -30.75714493]
     P_d = [-0.9832288, -17.28814125]
     P_f = [-7.75748634, -21.73072243]
-    yaw_0 = 177.7886658
-    yaw_d = 147.1214333
-    yaw_f = 89.86245729
+    yaw_0 = 177.7886658 - 90
+    yaw_d = 147.1214333 - 90
+    yaw_f = 89.86245729 + 90
 else:
     print("Invalid trial number")
     exit()
@@ -123,25 +123,23 @@ if trial != "0":
     x = df["LocationX"].to_list()
     y = df["LocationY"].to_list()
     yaw = df["RotationYaw"].to_list()
+    recorded_path = np.array([y, x])
 else:
-    x = [P_0[0], P_d[0], P_f[0]]
-    y = [P_0[1], P_d[1], P_f[1]]
-    yaw = [yaw_0, yaw_d, yaw_f]
-recorded_path = np.array([x, y])
+    recorded_path = None
 
 # Simulation Parameters
 # calculate the bezier path
 
 n_points = 50
 
-n_steps = 1
-speed = 0.5  # m/s
+n_steps = None
+speed = 1.0  # m/s
 init_steering_angle = 10  # abs value
 
 if (P_0 != [] and yaw_0 is not None) and (P_d != [] and yaw_d is not None):
     path1, control_points1, params1 = calculate_bezier_trajectory(
-        start_pos=P_0[::-1],
-        end_pos=P_d[::-1],
+        start_pos=P_0,
+        end_pos=P_d,
         start_yaw=yaw_0,
         end_yaw=yaw_d,
         n_points=n_points,
@@ -152,9 +150,9 @@ if (P_0 != [] and yaw_0 is not None) and (P_d != [] and yaw_d is not None):
     path1, trajectory1, yaw_angles_deg1 = simulation(
         path=path1,
         param=params1,
-        i_points=P_0[::-1],
-        f_points=P_d[::-1],
-        i_yaw=180 - yaw_0,
+        i_points=P_0,
+        f_points=P_d,
+        i_yaw=-(180 + yaw_0),
         speed=1 * speed,
         init_steering_angle=1 * init_steering_angle,
         vehicle_config=vehicle_config,
@@ -166,14 +164,12 @@ else:
     yaw_angles_deg1 = None
 
 print("=====================================")
-print("=====================================")
-print("=====================================")
 
 if (P_d != [] and yaw_d is not None) and (P_f != [] and yaw_f is not None):
     # backward so reverse the yaw angle (+180)
     path2, control_points2, params2 = calculate_bezier_trajectory(
-        start_pos=P_d[::-1],
-        end_pos=P_f[::-1],
+        start_pos=P_d,
+        end_pos=P_f,
         start_yaw=180 + yaw_d,
         end_yaw=180 + yaw_f,
         n_points=n_points,
@@ -185,9 +181,9 @@ if (P_d != [] and yaw_d is not None) and (P_f != [] and yaw_f is not None):
     path2, trajectory2, yaw_angles_deg2 = simulation(
         path=path2,
         param=params2,
-        i_points=P_d[::-1],
-        f_points=P_f[::-1],
-        i_yaw=180 - yaw_d,
+        i_points=P_d,
+        f_points=P_f,
+        i_yaw=-(yaw_d + 180), #  rotate and flip
         speed=-1 * speed,
         init_steering_angle=-1 * init_steering_angle,
         vehicle_config=vehicle_config,